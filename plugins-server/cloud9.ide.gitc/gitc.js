87551bddceb1016fabcf9683f7607e626e9e8a75
Markus Kahl <machisuji@googlemail.com>
2012-07-25 13:20:06 +0200

    live diff impl using bash script

--- plugins-server/cloud9.ide.gitc/gitc.js
+++ plugins-server/cloud9.ide.gitc/gitc.js
@@ -89,12 +89,7 @@ util.inherits(GitcPlugin, Plugin);
         message.extra.args = args;
         
         if (args[0] === "gitcdiff") {
-            if (args[1] === "writefile"){
-                return this.gitcdiff_write_command(user, message, client);
-            } else {
-                return this.gitcdiff_command(user, message, client);
-            }
-            //return true;
+            return this.gitcdiff_command(user, message, client);
         }
 
         // git encourages newlines in commit messages; see also #678
@@ -132,55 +127,16 @@ util.inherits(GitcPlugin, Plugin);
         return true;
     };
 
-    /**
-     * Special command: this will create a new file with the not yet saved
-     * file content. Thus we can later execute git diff with the original file and
-     * get the differences.
-     */
-    this.gitcdiff_write_command = function(user, message, client) {
-        var self = this;
-        //save tmp file to execute git diff
-        Fs.writeFile("~tmp.txt", message.extra.new_file_content, function (err) {
-            if (err) {
-                console.log("error writing tmp diff file");
-                self.error(err, 1, message, client);
-                return false;
-            }
-        });
-        
-        var args = ["echo", message.extra.success];
-        self.pm.spawn("shell", {
-            command: args[0],
-            args: args.slice(1),
-            cwd: message.cwd,
-            env: self.gitEnv,
-            extra: message.extra
-        }, self.channel, function(err, pid) {
-            if (err)
-                self.error(err, 1, message, client);
-        });
-        return true;
-    };
-
      /**
      * Special command: Executes git diff with the original file and the temporary file
      * created beforehand.
      */
     this.gitcdiff_command = function(user, message, client) {
         var self = this;
-        //execute git diff
-        var args = ["git", "diff", "--no-index"];
-        var msg_args = message.extra.args.slice(1); //remove gitcdiff
-        for (var i = 0; i < msg_args.length - 1; i++) { //append arguments
-            args.push(msg_args[i]);
-        }
-        args.push("--"); //append files to be diffed
-        args.push(msg_args[msg_args.length-1]);
-        args.push("/~tmp.txt");
 
         self.pm.spawn("shell", {
-            command: args[0],
-            args: args.slice(1),
+            command: "./plugins-server/cloud9.ide.gitc/git_temp_diff.sh",
+            args: [message.extra.args[message.extra.args.length-1], message.extra.new_file_content],
             cwd: message.cwd,
             env: self.gitEnv,
             extra: message.extra
@@ -189,18 +145,6 @@ util.inherits(GitcPlugin, Plugin);
                 self.error(err, 1, message, client);
         });
 
-        //remove tmp file
-        args = ["rm", "~tmp.txt"];
-        self.pm.spawn("shell", {
-            command: args[0],
-            args: args.slice(1),
-            cwd: message.cwd,
-            env: self.gitEnv,
-            extra: {nobroadcast: true}
-        }, self.channel, function(err, pid) {
-            if (err)
-                self.error(err, 1, message, client);
-        });
         return true;
     };
 
